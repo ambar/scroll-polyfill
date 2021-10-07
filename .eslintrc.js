7c65c5689e78dd81581ef0a8f53558ed731f249f
ambar <ambar.lee@gmail.com>
2021-10-07 16:45:55 +0800

    chore: update recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: [require.resolve('recommended/config/eslint')],
+  extends: ['@recommended/eslint-config'],
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,1 @@
-module.exports = require('recommended/config/prettier')
+module.exports = require('@recommended/prettier-config')
